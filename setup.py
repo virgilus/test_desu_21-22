ca923fd3f11d10ac1974d28deb0f58d7b20a6a7e
PESCE Virgile <virgile.PESCE@univ-amu.fr>
2022-05-18 14:51:45 +0200

    first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Ceci est un faux git pour s'entraÃ®ner !',
+    author='virgilus',
+    license='MIT',
+)
